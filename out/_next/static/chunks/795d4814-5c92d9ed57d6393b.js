16b76573511980c6004a0187cfd0a9daa169fd98
Mayank kadam <mayankkadam1039@gmail.com>
2024-07-23 01:36:41 +0530

    build fix

---
+++ out/_next/static/chunks/795d4814-5c92d9ed57d6393b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[51],{6356:function(t,n,a){a.d(n,{ddU:function(){return e}});var c=a(1810);function e(t){return(0,c.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0V0z"},child:[]},{tag:"path",attr:{d:"M9 16.2 4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"},child:[]}]})(t)}}}]);
